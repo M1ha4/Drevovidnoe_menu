14a51627b916c2e9cc2c991d1a450770f44e91a9
miha0 <miha04udalov@gmail.com>
2025-11-11 22:30:04 +0300

    Добавили html файлы страниц

--- drevovidnoe_menu/menu_app/urls.py
+++ drevovidnoe_menu/menu_app/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
+app_name = 'menu_app'
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('about/team/', views.team, name='team'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
 ]
--- drevovidnoe_menu/menu_app/views.py
+++ drevovidnoe_menu/menu_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
-def index():
-    pass
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def contact(request):
+    return render(request, 'contact.html')
